9cd54fa227e3449f308175168930ef6d4597f831
David Coss <David.Coss@stjude.org>
2013-12-19 09:43:53 -0600

    Added tests for punching in and summarizing.

--- setup.py
+++ setup.py
@@ -2,21 +2,35 @@
 
 from setuptools import setup, Command
 
+
 class Tester(Command):
     user_options = []
 
     def initialize_options(self):
+        import test
         import os
+        import os.path as OP
         self._dir = os.getcwd()
+
+        if OP.isfile(test.TEST_CLOCK_FILE):
+            os.remove(test.TEST_CLOCK_FILE)
 
     def finalize_options(self):
         pass
 
     def run(self):
         import test
-        print("Testing date2unix")
+        from time import sleep
 
         test.date2unix()
+        test.punchin()
+        print("Sleeping 2 seconds")
+        sleep(2)
+        test.switch_punch()
+        print("Sleeping 2 more seconds")
+        sleep(2)
+        test.punchout()
+        test.summary()
 
 
 the_scripts = ['scripts/punch', 'scripts/date2unix']
--- test/__init__.py
+++ test/__init__.py
@@ -1,11 +1,16 @@
+TEST_CLOCK_FILE = "clock.test.dat"
+
+
 def test_function(function):
     def tester():
+        print("Testing %s" % getattr(function, "__name__"))
         retval = function()
         if not retval:
                 print("FAILED")
                 exit(1)
         else:
             print("Success!")
+            print("")
         return retval
 
     return tester
@@ -16,13 +21,15 @@ def run_command(cmd):
     import subprocess
     return subprocess.check_output(shlex.split(cmd))
 
+
 @test_function
 def date2unix():
     import time
+    from timeclock import get_timestamp
     from datetime import datetime as DT
 
     # Get current time data
-    currunixtime = int(time.time())
+    currunixtime = get_timestamp()
     currdatetime = DT.fromtimestamp(currunixtime).strftime('%Y%m%d %H:%M')
     secoffset = DT.fromtimestamp(currunixtime).strftime('%S')
     currunixtime -= int(secoffset)
@@ -40,3 +47,28 @@ def date2unix():
     return retval
 
 
+@test_function
+def punchin():
+    print(run_command("punch -f %s test" % TEST_CLOCK_FILE))
+    return True
+
+
+@test_function
+def switch_punch():
+    print(run_command("punch -f %s -s test2" % TEST_CLOCK_FILE))
+    return True
+
+
+@test_function
+def punchout():
+    print(run_command("punch -f %s stop" % TEST_CLOCK_FILE))
+    return True
+
+
+@test_function
+def summary():
+    output = run_command("punch -f %s" % TEST_CLOCK_FILE)
+    print(output)
+    expected = "Total counted time: 4 seconds (0.00 hrs)"
+
+    return (expected in output)
